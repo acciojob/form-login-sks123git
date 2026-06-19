d2b353b257622bfd1bc1225061ba7598bf653daa
Sks <124144700+sks123git@users.noreply.github.com>
2026-06-19 15:57:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function getFormvalue() {
     //Write your code here
+ const fname = document.forms["form1"]["fname"].value.trim();
+    const lname = document.forms["form1"]["lname"].value.trim();
 
+    if (fname === "" || lname === "") {
+        alert("Please enter both first name and last name.");
+        return false;
+    }
+
+    const fullName = `${fname} ${lname}`;
+    alert(fullName);
+
+    return false; // Prevent form submission
 }
